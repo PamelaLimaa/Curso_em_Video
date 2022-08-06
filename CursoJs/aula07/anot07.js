6f2d1b5b0da2d35c235536bca3fa30633499873d
PamelaLimaa <layanepamela98@gmail.com>
2022-08-06 19:50:16 -0300

    aula07

---
+++ CursoJs/aula07/anot07.js
@@ -0,0 +1,40 @@
+//Operadores do Js 
+
+/*
+
+Aritmetricos, atribuição, relacionais, lógicos, ternário 
+
+Aritmetricos
++ - * % ** /
+
+Ordem de Precedência
+
+- ()
+- **
+- * / %
++ - 
+
+Incremento
+
+var x = 5
+x= x+1 // x++
+x= x-1 // x--
+
+Relacionais
+> maior
+< menor
+>= maior ou igual
+<= menor ou igual
+== igual
+!= diferente 
+
+logicos
+! - negação
+&& - conjunção ( and )
+|| - disjunção ( ou)
+
+ex:
+idade >= 15 && idade <= 17 // a idade está entre 15 e 17
+estado == 'RJ' || estado == 'SP' // o estado é RJ ou SP
+salario > 1500 && sexo != 'M' // o salario é acima de 1500 e não é homem?
+*/
