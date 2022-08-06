8d3854634804dbaab63c34dc993691b7e927e908
PamelaLimaa <layanepamela98@gmail.com>
2022-08-06 15:07:57 -0300

    anotações

---
+++ CursoJs/aula05/anot05.js
@@ -0,0 +1,31 @@
+// Tipos de Primitivos
+
+/* Identificadores
+- Podem cmeçar com letra, $ ou _
+- Não podem começar com números
+- É possível usar letras e números
+- É possível usar acentos e símbolos
+- Não podem conter espaços
+- Não podem ser palavras reservadas
+*/
+
+/* Dicas
+Maiúsculas e minúsculos fazem diferença
+
+Tenta escolher nomes coerentes para as variáveis
+
+Evite se tornar um 'programador alfabeto' ou um 'programador contador'
+
+typeof - retorna uma string indicando o tipo de um operando.
+
+*/
+
+/* Data Types
+- Number (Infinity - NaN)
+- String 
+- Boolean
+- Null
+- Undefined
+- Object (array)
+- Function
+*/
---
+++ CursoJs/aula06/anot06.js
@@ -0,0 +1,48 @@
+// Manipulação de Dados
+
+
+/* String > Number 
+
+convert number inteiro
+number.parseInt(n)
+
+convert number real
+number.parseFloat(n)
+
+sabe se virar é o foda
+Number(n)
+*/
+
+/* Número > String 
+
+String(n)
+
+n.toString()
+
+*/
+
+/* Formatando Strings
+
+Var s = 'JavaScript'
+
+'Eu estou aprendendo s' // não faz interpolação
+
+'Eu estou aprendendo' + s // usa concatenação
+
+`Eu estou aprendendo` ${s} // usa template string
+
+s.length //quantos caracteres a string tem
+s.toUpperCase() //tudo para maiusculas
+s.toLowerCase() //tudo para minusculas
+
+*/
+
+/* Formatando números
+
+var n1 = 1534.5
+
+n1.toFixed(2) // toFixed(2).replace('.', ',')
+
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+
+*/
