aad46ffbf75f7eafdce5e697ccd64717e6bbf5e7
PamelaLimaa <layanepamela98@gmail.com>
2022-10-23 18:39:24 -0300

    python/modulo02/

---
+++ python/modulo02/desafio004.py
@@ -0,0 +1,5 @@
+numero01 = int(input('Digite um número'))
+numero02 = int(input('Digite outro número'))
+soma = numero01 + numero02
+
+print('A soma entre {} e {} é igual a {}'.format(numero01, numero02, soma))
---
+++ python/modulo02/desafio005.py
@@ -0,0 +1,11 @@
+meuNome = input('Qual é o seu nome?')
+# minhaIdade = int(input('Qual é a sua idade?'))
+
+# print('Ola, seu nome é {} e a sua idade é {}, correto?'.format(meuNome,minhaIdade))
+
+# if meuNome == 'Pamela':
+#     print('É um prazer te ter você comigo novamente 💜')
+# else:
+#     print('Opa, é um prazer te conhecer {}, seu estranho :<'.format(meuNome))
+
+print(type(meuNome) is int)
