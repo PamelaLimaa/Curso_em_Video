47b925dc51763786da45e28a8e26cc194d88f38a
PamelaLimaa <layanepamela98@gmail.com>
2022-10-23 17:36:06 -0300

    python

---
+++ python/modulo01/desafio02.py
@@ -0,0 +1,5 @@
+dia = input('data do seu nascimento')
+mes = input('mês do seu nascimento')
+ano = input('ano do seu nascimento')
+
+print('Voce nasceu no',dia,mes,ano)
---
+++ python/modulo01/exer/exer002.py
@@ -0,0 +1,4 @@
+nome = input('Olá qual é o seu nome?')
+
+print('Boas vindas ', nome)
+print('Boas vindas, {}! '.format(nome))
